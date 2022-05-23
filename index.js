d58c78d9e0f00137445fffaa34a2f34fdf6f015f
broksainis <broksainis@gmail.com>
2022-05-24 01:11:55 +0300

    frontend styles

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const port = process.env.PORT || 8000;
 require('dotenv').config();
 const utils = require('./utils.js');
+const path = require('path');
 
 // app constants
 const APOLLO_KINO_EVENTS_URL = 'https://www.apollokino.lv/xml/Events?listType=NowInTheaters';
@@ -10,10 +11,14 @@ const APOLLO_KINO_SCHEDULE_URL = 'https://www.apollokino.lv/xml/Schedule/';
 
 const title = 'Kino Monolith';
 
+app.use("/css", express.static(path.join(__dirname, "node_modules/bootstrap/dist/css")));
+app.use("/js", express.static(path.join(__dirname, "node_modules/bootstrap/dist/js")));
+
 app.set('view engine', 'pug');
 app.get("/", async (req, res) => {
     try {
         const movies = await utils.mergeMoviesWithSchedule(APOLLO_KINO_EVENTS_URL, APOLLO_KINO_SCHEDULE_URL);
+        // console.log(movies)
         res.render('index', { title, movies });
     } catch (error) {
         console.error(error);
